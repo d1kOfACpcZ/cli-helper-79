6f32b765f60016e7ac58ccbf6a8084a8a2f6e2ae
d1kOfACpcZ <04041979.phuong@gmail.com>
2026-07-30 21:09:43 +0200

    logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,23 +1,36 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file='app.log', max_bytes=5*1024*1024, backup_count=3):
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
+# Logger setup with rotation
 
+def setup_logger(log_file='app.log', max_bytes=5*1024*1024, backup_count=5):
+    logger = logging.getLogger('my_logger')
+    logger.setLevel(logging.DEBUG)
+    
     # Create a rotating file handler
     handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
     handler.setLevel(logging.DEBUG)
-
+    
+    # Create a console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    
     # Create a logging format
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
-
-    # Add the handler to the logger
+    console_handler.setFormatter(formatter)
+    
+    # Add handlers to the logger
     logger.addHandler(handler)
-
+    logger.addHandler(console_handler)
+    
     return logger
 
+# Example usage of the logger
 if __name__ == '__main__':
-    log = setup_logger()
-    log.info('Logger has been set up successfully.')
+    logger = setup_logger()
+    logger.debug('This is a debug message')
+    logger.info('This is an info message')
+    logger.warning('This is a warning message')
+    logger.error('This is an error message')
+    logger.critical('This is a critical message')
