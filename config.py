c6fe590d59ec9c2f7ab3aba8da8e85252b06db49
Nils Andersen <04041979.phuong@gmail.com>
2026-08-06 21:09:11 +0200

    add configuration loader with defaults

--- config.py
+++ config.py
@@ -1,16 +1,32 @@
+import json
 import os
 
-class Config:
-    def __init__(self):
-        self.environment = os.getenv('ENVIRONMENT', 'development')
-        self.debug = self.environment == 'development'
-        self.database_uri = os.getenv('DATABASE_URI', 'sqlite:///default.db')
-        self.secret_key = os.getenv('SECRET_KEY', 'mysecret')
-        
-    def __str__(self):
-        return f'Config(environment={self.environment}, debug={self.debug}, database_uri={self.database_uri})'
+DEFAULT_CONFIG = {
+    'host': 'localhost',
+    'port': 8080,
+    'debug': False,
+    'log_level': 'INFO',
+}
 
-# Example of configuration loading
+class ConfigLoader:
+    def __init__(self, config_file='config.json'):
+        self.config_file = config_file
+        self.config = {}
+        self.load_config()
+
+    def load_config(self):
+        if os.path.exists(self.config_file):
+            with open(self.config_file, 'r') as file:
+                self.config = json.load(file)
+        else:
+            self.config = DEFAULT_CONFIG
+
+    def get(self, key, default=None):
+        return self.config.get(key, default)
+
+    def set(self, key, value):
+        self.config[key] = value
+
 if __name__ == '__main__':
-    config = Config()
-    print(config)
+    loader = ConfigLoader()
+    print(loader.get('host'))  # Example usage
